a6da38d219129f1e0e18f2cb4d47506486cae24c
TheSecEng <zachary.schulze@gmail.com>
2020-06-07 21:54:21 -0400

    initial commit

---
+++ docs/.vuepress/components/plans.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container class="grey lighten-5">
+        <v-row>
+            <v-col cols="6" md="4">
+                <v-card class="pa-2" tile>
+                    <v-img class="white--text" height="250px" src="./bronze.png">
+                    </v-img>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Number 10</span><br>
+                            <span>Whitehaven Beach</span><br>
+                            <span>Whitsunday Island, Whitsunday Islands</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn text color="orange">Share</v-btn>
+                        <v-btn text color="orange">Explore</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col cols="6" md="4">
+                <v-card class="pa-2" tile>
+                    <v-img class="white--text" height="250px" src="./silver.png">
+                    </v-img>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Number 10</span><br>
+                            <span>Whitehaven Beach</span><br>
+                            <span>Whitsunday Island, Whitsunday Islands</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn text color="orange">Share</v-btn>
+                        <v-btn text color="orange">Explore</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col cols="6" md="4">
+                <v-card class="pa-2" tile>
+                    <v-img class="white--text" height="250px" src="./gold.png">
+                    </v-img>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Number 10</span><br>
+                            <span>Whitehaven Beach</span><br>
+                            <span>Whitsunday Island, Whitsunday Islands</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn text color="orange">Share</v-btn>
+                        <v-btn text color="orange">Explore</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
---
+++ docs/README.md
@@ -0,0 +1,37 @@
+---
+home: true
+heroText: Bean's Bark Company
+heroImage: /logo.jpeg
+actionText: See our services below
+footer: Local to the PR Community
+---
+
+<plans />
+
+
+## What You Need to Know
+
+We walk your dog and pick up your dog’s poop. We will walk your dog for 30
+minutes, and it doesn’t take us more than 20 minutes to pick up your dog’s
+poop. Also, you have 3 choices: Bronze, Silver, or Gold membership. We are
+really responsible and will take good care of your dog’s when we walk your
+dogs.
+
+
+## Payment Policy
+
+**Payment Plan:** We will not allow debit card, credit card, check, or gift
+card. We will only allow cash. **No Payment:** If you don’t pay your bill when
+it is due then it will cost you $5 extra dollars each time you miss a week with
+the amount you need to pay each week. **Amount to Pay:** It depends on what
+membership you sign up for. If you sign up for the Bronze,
+then you pay $5 a week. If you sign up for the Silver, then you pay $10 a week.
+If you sign up for the Gold, then you pay $15.
+
+
+## Cancellation Policy
+
+**How to Cancel:** You call the number on the member card and give us the date
+on when you want to cancel the membership. **When You Cancel:** You are allowed
+to cancel the membership at any time of the membership. But you are not allowed
+to cancel it before the first week.
